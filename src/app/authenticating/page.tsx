3e009610b57c18fcbe225e2c227b05f021df6f32
SeakMengs <hseakmeng22@gmail.com>
2025-01-19 02:23:19 +0700

    Add test library and change http request fetch to axios

--- src/app/authenticating/page.tsx
+++ src/app/authenticating/page.tsx
@@ -1,18 +1,27 @@
 import { getApiBaseUrl } from "@/utils";
 import AuthenticationStatus from "./status";
+import { api } from "@/utils/axios";
+import { ResponseJson } from "@/types/response";
 
 type AuthenticatingProps = {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export type GoogleOAuthCallBackData = {
+  token: {
+    accessToken: string;
+    refreshToken: string;
+} | undefined;
 };
 
 export default async function Authenticating({
-  searchParams,
+    searchParams,
 }: AuthenticatingProps) {
-  const sp = await searchParams;
-  const urlSp = new URLSearchParams(sp as any);
-  const url = `${getApiBaseUrl()}/api/v1/oauth/google/callback?${urlSp.toString()}`;
-  const response = await fetch(url);
-  const data = await response.json();
+    const sp = await searchParams;
+    const urlSp = new URLSearchParams(sp as any);
+    const response = await api.get<ResponseJson<GoogleOAuthCallBackData>>(
+        `/api/v1/oauth/google/callback?${urlSp.toString()}`
+    );
 
-  return <AuthenticationStatus isOk={response.ok} token={data.data} />;
+    return <AuthenticationStatus isOk={response.status === 200} token={response.data.data.token} />;
 }
--- src/app/authenticating/status.tsx
+++ src/app/authenticating/status.tsx
@@ -2,74 +2,78 @@
 import { useEffect, useState } from "react";
 import { Spin, Alert, Button } from "antd";
 import { useRouter } from "next/navigation";
-import { clearRefreshAndAccessTokenCookie, setRefreshAndAccessTokenToCookie } from "@/utils";
+import {
+    clearRefreshAndAccessTokenCookie,
+    setRefreshAndAccessTokenToCookie,
+} from "@/utils";
+import { GoogleOAuthCallBackData } from "./page";
 
 type AuthenticationStatusProps = {
-  isOk: boolean;
-  token:
-    | {
-        accessToken: string;
-        refreshToken: string;
-      }
-    | undefined;
-};
+    isOk: boolean;
+} & GoogleOAuthCallBackData;
 
 export default function AuthenticationStatus({
-  isOk,
-  token,
+    isOk,
+    token,
 }: AuthenticationStatusProps) {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-  const router = useRouter();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const router = useRouter();
 
-  useEffect(() => {
-    if (isOk) {
-      // Simulate a short delay to show the loading state
-      setTimeout(async () => {
-        if (!token || !token.accessToken || !token.refreshToken) {
-          await clearRefreshAndAccessTokenCookie();
-          setError("Failed to authenticate.");
-          return;
+    useEffect(() => {
+        if (isOk) {
+            // Simulate a short delay to show the loading state
+            setTimeout(async () => {
+                if (!token || !token.accessToken || !token.refreshToken) {
+                    await clearRefreshAndAccessTokenCookie();
+                    setError("Failed to authenticate.");
+                    return;
+                }
+
+                await setRefreshAndAccessTokenToCookie(
+                    token.refreshToken,
+                    token.accessToken
+                );
+
+                setLoading(false);
+                router.push("/dashboard");
+            }, 1000);
+        } else {
+            setLoading(false);
+            setError("Failed to authenticate.");
         }
+    }, [isOk, router, token]);
 
-        await setRefreshAndAccessTokenToCookie(token.refreshToken, token.accessToken);
-
-        setLoading(false);
-        router.push("/dashboard");
-      }, 1000);
-    } else {
-      setLoading(false);
-      setError("Failed to authenticate.");
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <Spin size="large" />
+            </div>
+        );
     }
-  }, [isOk, router, token]);
 
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <Spin size="large" />
-      </div>
-    );
-  }
+    if (error) {
+        return (
+            <div className="flex items-center justify-center h-screen flex-col">
+                <Alert
+                    message="Authentication Error"
+                    description={error}
+                    type="error"
+                    showIcon
+                    style={{ marginBottom: 16 }}
+                />
+                <Button type="primary" onClick={() => router.push("/")}>
+                    Return to Home
+                </Button>
+                <Button
+                    type="primary"
+                    onClick={() => router.push("/api/oauth/google")}
+                >
+                    Retry
+                </Button>
+            </div>
+        );
+    }
 
-  if (error) {
-    return (
-      <div className="flex items-center justify-center h-screen flex-col">
-        <Alert
-          message="Authentication Error"
-          description={error}
-          type="error"
-          showIcon
-          style={{ marginBottom: 16 }}
-        />
-        <Button type="primary" onClick={() => router.push("/")}>
-          Return to Home
-        </Button>
-        <Button type="primary" onClick={() => router.push("/api/oauth/google")}>
-          Retry
-        </Button>
-      </div>
-    );
-  }
-
-  return null;
+    return null;
 }
