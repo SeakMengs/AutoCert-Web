709b4d86480b8df832c1952a714adc388f88fb67
SeakMengs <hseakmeng22@gmail.com>
2025-06-22 12:05:09 +0700

    Fix oauth redirect to localhost when hosting using cloudflare proxy

--- src/app/api/oauth/google/callback/route.ts
+++ src/app/api/oauth/google/callback/route.ts
@@ -1,7 +1,7 @@
+import { DOMAIN } from "@/utils";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams.toString();
-  const host = request.nextUrl.origin;
-  return Response.redirect(`${host}/authenticating?${searchParams}`);
+  return Response.redirect(`${DOMAIN}/authenticating?${searchParams}`);
 }
